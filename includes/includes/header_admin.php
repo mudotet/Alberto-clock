c5d88b942868a340f60c5406a3eacd095f0dbede
baanh2creator <baanhle24@gmail.com>
2025-06-09 20:54:30 +0700

    test

---
+++ includes/includes/header_admin.php
@@ -0,0 +1,11 @@
+<!-- includes/header_admin.php -->
+<header>
+  <nav class="navbar navbar-expand-lg shadow-sm" style="background-color: #8B4000;">
+    <div class="container">
+      <a class="navbar-brand text-white fw-bold" href="#">
+        <img src="/Alberto-clock/assets/images/logo.png" alt="Alberto Clock" height="50" style="width: auto;">
+        <span class="ms-2 fs-5">Trang quản trị</span>
+      </a>
+    </div>
+  </nav>
+</header>
