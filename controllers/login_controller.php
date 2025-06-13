2379a338eab12f94d2665060f76e235913c5de7b
Tus <mudotet@gmail.com>
2025-06-13 20:39:04 +0700

    sửa lại login cũ với header cũ

--- controllers/login_controller.php
+++ controllers/login_controller.php
@@ -10,17 +10,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $userModel = new User();
         $user = $userModel->authenticate($email, $password);
 
-        if ($user) {
-            // Đăng nhập thành công
-            $_SESSION['user'] = [
-                'id'      => $user['user_id'],
-                'name'    => $user['name'],
-                'email'   => $user['email'],
-                'role_id' => $user['role_id']
-            ];
-            header("Location: ../views/index.php");
-            exit();
+    if ($user) {
+        // Đăng nhập thành công
+        $_SESSION['user'] = [
+            'id'      => $user['user_id'],
+            'name'    => $user['name'],
+            'email'   => $user['email'],
+            'role_id' => $user['role_id']
+        ];
+        if ($user['role_id'] == 1) {
+            header("Location:../views/users_curd_page.php");
         } else {
+            header("Location: ../views/index.php");
+        }
+        exit();
+    } else {
             // Sai thông tin đăng nhập
             echo "<script>alert('Email hoặc mật khẩu không đúng!'); window.location.href='../views/login.php';</script>";
             exit();
